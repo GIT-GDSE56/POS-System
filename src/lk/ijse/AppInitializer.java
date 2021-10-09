a9d33116e1bb4b6f6b86811faccc0216bc103b81
MG4ACA <mg4.aca@gmail.com>
2021-10-09 10:57:27 +0530

    initial commit

---
+++ src/lk/ijse/AppInitializer.java
@@ -0,0 +1,19 @@
+package lk.ijse;/**
+ * @author MG4ACA <mg4.aca@gmail.com>
+ * @since 10/9/2021
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
